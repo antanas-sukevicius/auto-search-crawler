33efaa48f6f9e465743e93c651b425df5fd8cfb3
unknown <antanas.sukevicius@gmail.com>
2021-06-07 19:46:48 +0300

    remove multiple files

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const fs = require('fs');
     const carList = await cars.getCarListFromPage("https://autogidas.lt/skelbimai/automobiliai/?f_1%5B0%5D=" + carModel)
     for (let p = 0; p < carList.length; p++) {
         const pageList = carList[p];
-        
+
         console.log("parsing data");
 
         for (let c = 0; c < pageList.length; c++) {
@@ -21,7 +21,7 @@ const fs = require('fs');
         }
     }
 
-    fs.writeFileSync(carModel+"-data.json", JSON.stringify(carList, null, 4));
+    fs.writeFileSync("./data/" + carModel+"-data.json", JSON.stringify(carList, null, 4));
     console.log("completed");
     
 }
