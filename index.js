cc6b27cb30774a838751a2d784e4aabf19ae2629
unknown <antanas.sukevicius@gmail.com>
2021-06-07 19:33:49 +0300

    test

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const fs = require('fs');
     const carList = await cars.getCarListFromPage("https://autogidas.lt/skelbimai/automobiliai/?f_1%5B0%5D=" + carModel)
     for (let p = 0; p < carList.length; p++) {
         const pageList = carList[p];
+        
         console.log("parsing data");
 
         for (let c = 0; c < pageList.length; c++) {
